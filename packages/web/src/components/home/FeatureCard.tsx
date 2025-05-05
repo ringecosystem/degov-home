8bebe9b7eb4aa373d0acd4132ef17604d9e3cd66
snoopy1412 <snoopy1412.su@gmail.com>
2025-05-05 18:35:07 +0800

    Add GlowingEffect component

--- packages/web/src/components/home/FeatureCard.tsx
+++ packages/web/src/components/home/FeatureCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useScrollAnimation } from '@/hooks/useScrollAnimation';
+import { GlowingEffect } from '@/components/ui/glowing-effect';
 interface FeatureCardProps {
   title: string;
   description: string;
@@ -13,10 +14,19 @@ export default function FeatureCard({ title, description, icon, index }: Feature
   });
   return (
     <div
-      className="flex flex-col gap-[20px] border-[2px] border-[#474747] px-[20px] pt-[140px] pb-[20px] lg:px-[40px] lg:pt-[170px] lg:pb-[40px]"
+      className="relative flex flex-col gap-[20px] rounded-sm border-[2px] border-[#474747] px-[20px] pt-[140px] pb-[20px] lg:px-[40px] lg:pt-[170px] lg:pb-[40px]"
       ref={ref}
       style={animatedStyles}
     >
+      <GlowingEffect
+        blur={0}
+        borderWidth={3}
+        spread={80}
+        glow={true}
+        disabled={false}
+        proximity={64}
+        inactiveZone={0.01}
+      />
       <Image src={icon} alt={title} width={160} height={160} className="hidden lg:inline-block" />
       <Image src={icon} alt={title} width={120} height={120} className="inline-block lg:hidden" />
 
