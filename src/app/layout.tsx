7d22d438ab74b1503a333b855c7d2a9ff35100a9
snoopy1412 <snoopy1412.su@gmail.com>
2025-05-09 10:48:39 +0800

    Add Open Graph metadata generation and include OG image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,46 @@ export const metadata: Metadata = {
     'DeGov.AI is a AI-powered platform for decentralized governance, built on the Openzeppelin contracts.'
 };
 
+export async function generateMetadata(): Promise<Metadata> {
+  const name = 'DeGov.AI';
+  const description =
+    'DeGov.AI is a AI-powered platform for decentralized governance, built on the Openzeppelin contracts.';
+  const siteUrl = 'https://degov.ai';
+  const ogImageUrl = `${siteUrl}/image/og.png`;
+
+  return {
+    title: name,
+    description,
+    metadataBase: new URL(siteUrl),
+    openGraph: {
+      type: 'website',
+      siteName: name,
+      title: name,
+      description,
+      url: siteUrl,
+      images: [
+        {
+          url: ogImageUrl,
+          width: 512,
+          height: 512,
+          alt: name
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary',
+      site: '@ai_degov',
+      creator: '@ai_degov',
+      title: name,
+      description,
+      images: [ogImageUrl]
+    },
+    other: {
+      timestamp: Date.now()
+    }
+  };
+}
+
 export default function RootLayout({
   children
 }: Readonly<{
